66062b94ac2914d535c1db5938b77d30063f62de
perouz <git@perouz.com>
2016-02-09 17:15:23 -0500

    Add classes Chain and Polygon.

--- geothings.py
+++ geothings.py
@@ -3,7 +3,8 @@ import math
 import sys
 
 
-class Point (object):
+class Point(object):
+    """Euclid: A point is that which has no part."""
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
@@ -18,6 +19,7 @@ class Point (object):
         return hash((self.x, self.y))
 
     def line_through_point(self, p):
+	"""Returns the line through two points."""
         # Is a vertical line
         if (p.x == self.x):
             return Line(1, 0, p.x)
@@ -34,28 +36,37 @@ class Point (object):
             print "Unexpected Error:", sys.exc_info()
             raise
 
-    # returns true if the given point is on line l
     def on_line(self, l):
+    	"""Returns true if the given point is on line l"""
         try:
             return ((l.a * self.x) + (l.b * self.y) == l.c)
         except:
             print "Unexpected Error:", sys.exc_info()
             raise
 
-    # returns the mirror reflection of self across a line p1,
-    # or a line through p1 and p2
     def mirror_point(self, p1=None, p2=None):
+   	"""Returns the mirror reflection of self across a line p1,
+    	    or a line through p1 and p2."""
         L = p1 if (p2 is None) else Line(p1, p2)
         perpL = L.perpendicular_through_point(self)
         p = L.point_of_intersection(perpL)
         return Point(2*p.x - self.x, 2*p.y - self.y)
 
+    def move(self,x,y):
+	"""Moves the point to the new (x,y) position."""
+	self.x = x
+	self.y = y
 
-# A line is defined by two points and described by the equation ax + by = c
-# You can create one by passing the values a, b, c of the equation
-# OR by passing it two points
-class Line (object):
+
+
+class Line(object):
+    """Euclid: A line is breadthless length.""" 
     def __init__(self, a=None, b=None, c=None):
+	"""A line is defined by two points and described by the equation ax + by = c 
+
+	   You can create one by passing the values a, b, c of the equation 
+	   OR by passing it two points.
+	"""
         # if line is defined by two points a and b
         if (c is None):
             # check for a vertical line
@@ -91,6 +102,9 @@ class Line (object):
         else:
             self.slope == that.slope and self.intercept == that.intercept
 
+    def __hash__(self):
+        return hash((self.a, self.b, self.c))
+
     @property
     def is_well_defined(self):
         return (self.a and self.a != 0) or (self.b and self.b != 0)
@@ -117,21 +131,21 @@ class Line (object):
     def is_parallel(self, l):
         return True if (self.slope == l.slope) else False
 
-    # returns true if the two lines are the same
     def is_overlapping(self, l):
+    	"""Returns true if the two lines are the same."""
         return True if (self.slope == l.slope) and \
             (self.intercept == l.intercept) \
             else False
 
-    # equation of line through p that is
-    # parallel to self: L ': ax + by = a*p.x + b*p.y
     def parallel_through_point(self, p):
+    	"""Computes the equation of the line through p that is
+    	   parallel to self: L ': ax + by = a*p.x + b*p.y."""
         return Line(1, 0, p.x) if self.is_vertical \
             else Line(self.a, self.b, (self.a * p.x) + (self.b * p.y))
 
-    # equation of line through p that is
-    # perpendicular to  self: L': -abx + ay = -b*p.x + a*p.y
     def perpendicular_through_point(self, p):
+    	"""Computes the equation of the line through p that is
+    	   perpendicular to  self: L': -abx + ay = -b*p.x + a*p.y."""
         if self.is_horizontal:
             return Line(1, 0, p.x)
         else:
@@ -140,8 +154,8 @@ class Line (object):
             c = (self.a * p.y) - (self.b * p.x)
             return Line(a, b, c)
 
-    # return the intersection point with another line
     def point_of_intersection(self, that):
+    	"""Returns the intersection point with another line."""
         # if lines are parallel return None
         if (self.is_parallel(that)):
             return None
@@ -156,3 +170,52 @@ class Line (object):
                 (that.a/that.b - self.a/self.b)
             y = that.c/that.b - that.a/that.b*x
             return Point(x, y)
+
+
+class Chain(object):
+    def __init__(self, points):
+	"""An open polygonal chain is formed by an ordered list of points CW."""
+        self.points = points
+        self.n = len(points)
+
+    def __hash__(self):
+        return hash(self.points)
+
+    def __str__(self):
+        point_str = ""
+        for point in self.points:
+            if point_str:
+                point_str += ", "
+            point_str += str(point)
+        return point_str
+
+    def next(self,current_point):
+	"""Returns the next point (in CW direction) along the chain.
+	   If the current point is the last point or does not exist, returns null.
+	"""
+	if not current_point in self.points:
+	    return None
+	else: 
+	    current_index = self.points.index(current_point)
+
+	# if current point is the last in the list return None
+	return None if current_index == self.n-1 else self.points[current_index+1]	
+
+
+    def previous(self,current_point):
+	"""Returns the previous point (in CW direction) along the chain.
+	   If the current point is the first point or does not exist, returns null.
+	"""
+	if not current_point in self.points:
+	    return None
+	else: 
+	    current_index = self.points.index(current_point)
+	# if current point is the first in the list return None
+	return None if current_index == 0 else self.points[current_index-1]	
+
+
+class Polygon(Chain):
+    """"A polygon is a closed chain."""
+    def __init__(self, points):
+	Chain.__init__(self, points)
+
